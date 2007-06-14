4a39611a323995be0573ec13a3c39763223b15b3
GrAnd <GrAnd@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2007-06-14 19:43:30 +0000

    #126 * S-series: Fixed get_focus command
    git-svn-id: https://tools.assembla.com/svn/chdk/branches/grand@171 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/s2is/wrappers.c
+++ platform/s2is/wrappers.c
@@ -1,15 +1,14 @@
 #include "../generic/wrappers.c"
 #include "keyboard.h"
+#include "conf.h"
 #include "platform.h"
 
 long lens_get_focus_pos()
 {
-    static long v=65535;
-    return (kbd_is_key_pressed(KEY_SHOOT_HALF))?v=_GetFocusLensSubjectDistance():v;
+    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || state_kbd_script_run)?_GetFocusLensSubjectDistance():65535;
 }
 
 long lens_get_target_distance()
 {
-    static long v=65535;
-    return (kbd_is_key_pressed(KEY_SHOOT_HALF))?v=_GetCurrentTargetDistance():v;
+    return (kbd_is_key_pressed(KEY_SHOOT_HALF) || state_kbd_script_run)?_GetCurrentTargetDistance():65535;
 }
